c7dc6850da9d5b4e281d45ce0de03526c212a5f9
Claudio Atilano <claudio_atilano@hotmail.com>
2017-02-17 23:45:56 +0000

    Debugged third column, now you can only post from your portfolio or from home, not other users portfolio

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,9 +29,9 @@ class PagesController < ApplicationController
     end
     
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
-    
+  
     @newPost = Post.new
-    
+  
     @toFollow = User.all.last(5)
   end
 
