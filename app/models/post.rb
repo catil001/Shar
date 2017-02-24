66af76d68be8c92ee78d62acb1e7c0bff9395852
Claudio Atilano <claudio_atilano@hotmail.com>
2017-02-24 05:10:15 +0000

    Change twitter to pinterest

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 } #max is 140 characters, this can be changed for future..
   
   #for images on posts
-  has_attached_file :image, styles: {large: "600x600", medium: "300x300>", thumb: "150x150>" }
+  has_attached_file :image, styles: {large: "500x800>", medium: "400x400>", thumb: "150x150>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   default_scope -> { order(created_at: :desc) } #newests posts first
 end
--- db/migrate/20170210090541_add_username_to_users.rb
+++ db/migrate/20170210090541_add_username_to_users.rb
@@ -1,7 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change #Anything inside of this is changed to SQL type database
-    add_column :users, :username, :string # add a new column to the table "users" now called "usernames"
-    add_index :users, :username, unique: true #first index usernames for quck lookup. Ensure usernames are always unique
-     
+    add_column :users, :username, :string # add a new column to the table "users" now called "userusernames"
+    add_index :users, :username, unique: false #first index userusernames for quck lookup. Ensure userusernames are always unique
   end
 end
